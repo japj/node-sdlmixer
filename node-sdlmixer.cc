46cd9282bf004fe18516f1e2b2d83034ef79a7a1
Jeroen Janssen <jeroen.janssen@gmail.com>
2011-05-09 08:01:28 +0200

    first go at a synch play() function

--- node-sdlmixer.cc
+++ node-sdlmixer.cc
@@ -4,9 +4,40 @@
 
 using namespace v8;
 
+
+#define NODE_SET_METHOD(obj, name, callback)                              \
+  obj->Set(v8::String::NewSymbol(name),                                   \
+           v8::FunctionTemplate::New(callback)->GetFunction())
+
 static int still_playing(void)
 {
   return(Mix_Playing(0));
+}
+
+static Handle<Value> Play(const Arguments& args) {
+  HandleScope scope;
+  
+  Mix_Chunk *wave = NULL;
+
+  if (args.Length() < 1) {
+    return ThrowException(Exception::TypeError(String::New("Bad argument")));
+  }
+
+  /* Load the requested wave file */
+  String::AsciiValue fileName(args[0]->ToString());
+  printf("Loading %s\n",*fileName);
+  wave = Mix_LoadWAV(*fileName);
+
+  printf("Playing\n");
+  /* Play and then exit */
+  Mix_PlayChannel(0, wave, 0);
+
+  while (still_playing()) {
+    SDL_Delay(1);
+
+  } /* while still_playing() loop... */
+  printf("Done\n");
+  return scope.Close(args[0]);
 }
 
 
@@ -15,6 +46,7 @@ init (Handle<Object> target)
 {
   HandleScope scope;
   target->Set(String::New("hello"), String::New("world"));
+  NODE_SET_METHOD(target, "play", Play);
 
   /* Initialize the SDL library */
   if ( SDL_Init(SDL_INIT_AUDIO) < 0 ) {
@@ -25,8 +57,6 @@ init (Handle<Object> target)
   int audio_rate;
   Uint16 audio_format;
   int audio_channels;
-  Mix_Chunk *wave = NULL;
-
 
   /* Initialize variables */
   audio_rate = MIX_DEFAULT_FREQUENCY;
@@ -44,17 +74,5 @@ init (Handle<Object> target)
 			(audio_channels > 2) ? "surround" :
 			(audio_channels > 1) ? "stereo" : "mono");
 
-    /* Load the requested wave file */
-    wave = Mix_LoadWAV("./wavs/alarm.wav");
-
-    /* Play and then exit */
-    Mix_PlayChannel(0, wave, 0);
-
-    while (still_playing()) {
-      SDL_Delay(1);
-
-    } /* while still_playing() loop... */
-
-
-  }
+    }
 }
--- test.js
+++ test.js
@@ -3,4 +3,6 @@ var sdlmixer = require("./build/default/node-sdlmixer"),
         puts = sys.puts;
 
 puts(sdlmixer.hello);
+sdlmixer.play("./wavs/alarm.wav");
+sdlmixer.play("./wavs/alarm.wav");
 
